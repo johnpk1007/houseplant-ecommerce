cd9f3c7bca4983af834ef7d96a1d2f46e58e7cae
johnpk1007 <112378566+johnpk1007@users.noreply.github.com>
2026-01-20 18:52:25 -0500

    sign in and sign up

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,16 +3,16 @@ import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService){}
+export class AuthController {
+    constructor(private authService: AuthService) { }
 
     @Post('signup')
-    signup(@Body() dto:AuthDto){
+    signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto)
     }
 
     @Post('signin')
-    signin(){
-       return this.authService.signin()
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto)
     }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,22 +1,48 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as bcrypt from "bcrypt"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
 @Injectable()
-export class AuthService{
-    constructor(private prisma : PrismaService){}
-    async signup(dto:AuthDto){
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDto) {
         const hash = await bcrypt.hash(dto.password, 10);
-        const user = await this.prisma.customer.create({
-            data:{
-                email:dto.email,
-                hash,
+        try {
+            const customer = await this.prisma.customer.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+            return customer
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credential taken')
+                }
             }
-        })
-        return user
+            throw error
+        }
+
+
     }
-    signin(){
-        return {msg:'I have signed in'}
+    async signin(dto: AuthDto) {
+        const customer = await this.prisma.customer.findUnique({
+            where: { email: dto.email }
+        })
+        if (!customer) {
+            throw new ForbiddenException(
+                'Credentials incorrect'
+            )
+        }
+        const pwMatches = await bcrypt.compare(dto.password, customer.hash)
+        if (!pwMatches) {
+            throw new ForbiddenException(
+                'Credentials incorrect'
+            )
+        }
+        return customer
     }
 }
