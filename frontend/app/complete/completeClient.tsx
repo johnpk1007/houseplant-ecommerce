8d084411717319b97f2f973217717c85a75b8723
johnpk1007 <112378566+johnpk1007@users.noreply.github.com>
2026-03-17 15:39:41 -0400

    fixed error

--- frontend/app/complete/completeClient.tsx
+++ frontend/app/complete/completeClient.tsx
@@ -15,7 +15,7 @@ type OrderItem = {
 }
 
 type OrderType = {
-    status: 'PENDING' | 'PAID' | 'FAILED';
+    orderStatus: 'PENDING' | 'PAID' | 'FAILED';
     orderItems: OrderItem[];
 }
 
@@ -72,7 +72,7 @@ export default function CompleteClient({ paymentIntentId }: { paymentIntentId: s
             <div className="flex flex-col 1700px:h-[1000px] 500px:h-[700px] h-screen w-full">
                 <div className="w-full flex-1 500px:block hidden" />
                 <div className="w-full 500px:h-[30%] h-full relative">
-                    <LoadingImageWithImageData imageData={order.status === 'PAID' ? Complete : Unclompete} />
+                    <LoadingImageWithImageData imageData={order.orderStatus === 'PAID' ? Complete : Unclompete} />
                     <div className="absolute top-0 left-0 flex flex-col w-full pt-[20px] pl-[40px]">
                         <div className={`970px:top-[15px] 500px:top-[8px] top-[50px] left-[40px] z-3 ${maxPage === 0 && 'hidden'}`}>
                             <button className={`text-[#ADADAD] w-[20px] h-[20px] duration-300 ease-in-out ${page === 0 ? 'text-[#ADADAD]' : 'text-[#E2E2E2] hover:text-[#E2E2E2]/70 cursor-pointer'}`} onClick={handlePrev}><Left /></button>
@@ -113,10 +113,10 @@ export default function CompleteClient({ paymentIntentId }: { paymentIntentId: s
                     <div className="w-full h-full absolute top-0 left-0 bg-black/40 items-end 500px:hidden flex">
                         <div className="w-full h-3/7 flex flex-col justify-start items-end pt-[30px] px-[20px]">
                             <div className="font-playfairDisplay text-[30px] text-right text-balance w-full text-white/70 mb-[30px]">
-                                {order.status === 'PAID' ? 'Your Order is Confirmed and Processing.' : 'Your order was not Completed.'}
+                                {order.orderStatus === 'PAID' ? 'Your Order is Confirmed and Processing.' : 'Your order was not Completed.'}
                             </div>
                             <div className="font-playfairDisplay text-[14px] text-right text-balance w-full text-white/70">
-                                {order.status === 'PAID' ?
+                                {order.orderStatus === 'PAID' ?
                                     'We are so grateful to welcome you to our community. Your new plants are now being prepared by our expert growers and will be shipped with the utmost care. We encourage you to begin preparing your space and gathering inspiration to truly embrace your slow living journey.'
                                     :
                                     'Unfortunately, we were unable to process your order. Please review your payment details or try again. If the issue persists, feel free to contact our support team for assistance.'}
@@ -127,10 +127,10 @@ export default function CompleteClient({ paymentIntentId }: { paymentIntentId: s
                 <div className="w-full 1700px:flex-1 flex-2 flex flex-row justify-end 500px:block hidden">
                     <div className="flex flex-col justify-start items-end 1700px:pt-[80px] pt-[30px] 1700px:pr-[100px] 750px:pr-[50px] px-[20px]">
                         <div className="font-playfairDisplay 1700px:text-[40px] text-[30px] text-right text-balance w-[500px] 1700x:mb-[60px] mb-[30px]">
-                            {order.status === 'PAID' ? 'Your Order is Confirmed and Processing.' : 'Your order was not Completed.'}
+                            {order.orderStatus === 'PAID' ? 'Your Order is Confirmed and Processing.' : 'Your order was not Completed.'}
                         </div>
                         <div className="font-playfairDisplay 1700px:text-[16px] text-[14px] text-right text-balance 750px:w-[700px] w-full text-[#ADADAD]">
-                            {order.status === 'PAID' ?
+                            {order.orderStatus === 'PAID' ?
                                 'We are so grateful to welcome you to our community. Your new plants are now being prepared by our expert growers and will be shipped with the utmost care. We encourage you to begin preparing your space and gathering inspiration to truly embrace your slow living journey.'
                                 :
                                 'Unfortunately, we were unable to process your order. Please review your payment details or try again. If the issue persists, feel free to contact our support team for assistance.'}
