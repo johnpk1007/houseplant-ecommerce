b9a72090c7aa02d52ac47cc14bab65b1c7f1f42d
johnpk1007 <112378566+johnpk1007@users.noreply.github.com>
2026-03-16 21:53:24 -0400

    fixed cookie error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser'
-import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
@@ -16,26 +15,6 @@ async function bootstrap() {
   });
   app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log('--- Request ---');
-    console.log('URL:', req.originalUrl);
-    console.log('Method:', req.method);
-    console.log('Cookies:', req.cookies);
-    console.log('Headers:', req.headers);
-
-    const originalSend = res.send.bind(res);
-    res.send = ((body: any) => {
-      console.log('--- Response ---');
-      console.log('Status:', res.statusCode);
-      console.log('Headers:', res.getHeaders());
-      console.log('Set-Cookie:', res.getHeader('Set-Cookie'));
-      console.log('Body:', body);
-      return originalSend(body);
-    }) as typeof res.send;
-
-    next();
-  });
 
   await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
