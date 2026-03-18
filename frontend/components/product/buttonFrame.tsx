3960afedc00472952b9fc21e6735cf6066259200
johnpk1007 <112378566+johnpk1007@users.noreply.github.com>
2026-03-18 15:38:58 -0400

    fix error

--- frontend/components/product/buttonFrame.tsx
+++ frontend/components/product/buttonFrame.tsx
@@ -8,7 +8,7 @@ import { Product } from "@/types/product";
 export default function ButtonFrame({ productData }: { productData: Product }) {
     const [cartQuantity, setCartQuantity] = useState(1)
     return (
-        <div className="absolute left-5 500px:bottom-5 bottoom-10 flex flex-col z-2">
+        <div className="absolute left-5 500px:bottom-5 bottom-10 flex flex-col z-2">
             <QuantityButton cartQuantity={cartQuantity} setCartQuantity={setCartQuantity} />
             <CartButton product={productData} cartQuantity={cartQuantity} />
         </div>
