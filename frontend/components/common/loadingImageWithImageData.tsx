08aa47276071ff2cde27e5ad4e2f29421b47885d
johnpk1007 <112378566+johnpk1007@users.noreply.github.com>
2026-03-18 09:31:01 -0400

    image test

--- frontend/components/common/loadingImageWithImageData.tsx
+++ frontend/components/common/loadingImageWithImageData.tsx
@@ -5,15 +5,26 @@ import { useState } from "react"
 
 export default function LoadingImageWithImageData({ imageData, fetchpriority }: { imageData: StaticImageData, fetchpriority?: "high" }) {
     const [isLoaded, setIsLoaded] = useState(false)
-    return <div className="-z-1 relative w-full h-full">
-        {!isLoaded && <div className="bg-gray-200 w-full h-full animate-pulse"></div>}
-        <Image
-            src={imageData}
-            alt="product"
-            fill
-            className={`object-cover ${isLoaded ? "opacity-100" : "opacity-0"} transition-opacity duration-300 ease-in-out`}
-            onLoad={() => setIsLoaded(true)}
-            {...(fetchpriority ? { fetchPriority: fetchpriority } : {})}
-        />
-    </div>
+    return (
+        <div
+            className={`-z-1 relative w-full h-full`}
+        >
+            {!fetchpriority && !isLoaded && <div className="bg-gray-200 w-full h-full animate-pulse"></div>}
+            <Image
+                src={imageData}
+                alt="product"
+                fill
+                className={`object-cover 
+                    ${fetchpriority === "high"
+                        ? ""
+                        : isLoaded
+                            ? "opacity-100"
+                            : "opacity-0"
+                    } 
+                    transition-opacity duration-300 ease-in-out`}
+                onLoad={() => setIsLoaded(true)}
+                priority={fetchpriority === "high"}
+                {...(fetchpriority ? { fetchPriority: fetchpriority } : {})}
+            />
+        </div>)
 }
